4bcd87c5f2c17dc3bafa140a50310e4709ca8a5c
Trou95 <gorkem_095@hotmail.com>
2025-08-19 00:37:01 +0300

    Added shadcn

---
+++ frontend/src/app/(home)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function HomeLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <div className="flex-1">{children}</div>
+    </div>
+  );
+}
