86ff45da0bd3cae68320f64fb897bc51a22a984a
Trou95 <gorkem_095@hotmail.com>
2025-08-19 15:11:37 +0300

    Add sidebar component with navigation and user menu

---
+++ frontend/src/components/radix-ui/sidebar/app-sidebar.tsx
@@ -0,0 +1,61 @@
+import {
+  Sidebar,
+  SidebarContent, SidebarFooter,
+  SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { Container, Home, Package } from 'lucide-react';
+import { NavUser } from '@/components/radix-ui/sidebar/nav-user';
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Categories",
+    url: "/categories",
+    icon: Container,
+  },
+  {
+    title: "Products",
+    url: "/products",
+    icon: Package,
+  },
+]
+
+const user = {
+  name: "shadcn",
+  email: "m@example.com",
+  avatar: "/avatars/shadcn.jpg",
+}
+
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel className="text-2xl mb-2">Products.API</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
