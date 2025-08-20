d3995487cea7effddf72445507f38005e59553bc
Trou95 <gorkem_095@hotmail.com>
2025-08-20 15:39:52 +0300

    fix Dockerfile and entrypoint script; update migration files and database context

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+echo "Applying migrations..."
+# Migration'ları çalıştır
+dotnet tool install --global dotnet-ef || true
+export PATH="$PATH:/root/.dotnet/tools"
+dotnet ef database update -p Kayra.Api/Kayra.Api.csproj -s Kayra.Api/Kayra.Api.csproj
 
-echo "Starting application with automatic migration..."
+echo "Starting Kayra.Api..."
 exec dotnet Kayra.Api.dll
